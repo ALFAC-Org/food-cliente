2ede7d0efbf505f30cc4f8abd9fabee4f48a8dab
Leonardo Fraga <lafraga91@gmail.com>
2024-05-26 14:21:13 -0300

    Cria validação da categoria

--- food-api/src/main/java/br/com/alfac/food/api/config/FoodExceptionHandler.java
+++ food-api/src/main/java/br/com/alfac/food/api/config/FoodExceptionHandler.java
@@ -1,20 +1,24 @@
 package br.com.alfac.food.api.config;
 
-import br.com.alfac.food.api.config.exception.ApiError;
-import br.com.alfac.food.api.config.exception.ApiErrorItem;
-import br.com.alfac.food.core.exception.FoodException;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
+import java.util.Iterator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Iterator;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import br.com.alfac.food.api.config.exception.ApiError;
+import br.com.alfac.food.api.config.exception.ApiErrorItem;
+import br.com.alfac.food.core.exception.FoodException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class FoodExceptionHandler {
@@ -32,13 +36,33 @@ public class FoodExceptionHandler {
         return buildResponseEntity(error);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiError> httpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        LOGGER.error(ex.getLocalizedMessage(), ex);
+        var status = HttpStatus.INTERNAL_SERVER_ERROR.value();
+        var message = "Ops, ocorreu um erro interno.";
+        var messageDefaultError = "INTERNAL_SERVER_ERROR";
+
+         if(ex.getCause() instanceof InvalidFormatException) {
+            InvalidFormatException invalidFormatException = (InvalidFormatException) ex.getCause();
+            status = HttpStatus.BAD_REQUEST.value();
+            message = invalidFormatException.getMessage();
+            messageDefaultError = "BAD_REQUEST";
+         }
+        ApiError error = new ApiError(
+                status,
+                message,
+                messageDefaultError
+        );
+        return buildResponseEntity(error);
+    }
+
     @ExceptionHandler(FoodException.class)
     public ResponseEntity<ApiError> handleFoodException(FoodException ex) {
         LOGGER.error(ex.getLocalizedMessage(), ex);
         ApiError error =ApiError.createError(ex);
         return buildResponseEntity(error);
     }
-
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ApiError> handleConstraintViolationException(ConstraintViolationException ex) {
@@ -50,7 +74,6 @@ public class FoodExceptionHandler {
             ConstraintViolation error = (ConstraintViolation) var4.next();
             apiError.getArguments().add(new ApiErrorItem("", error.getMessage(), error.getPropertyPath().toString()));
         }
-
 
         return buildResponseEntity(apiError);
     }
@@ -67,8 +90,6 @@ public class FoodExceptionHandler {
 
         return buildResponseEntity(apiError);
     }
-
-
 
     private ResponseEntity<ApiError> buildResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, HttpStatus.valueOf(apiError.getStatus()));
