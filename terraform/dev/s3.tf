e71c9544b92f233b4c16f31366a1f9c8db73da89
carlohcs <carlohcs@gmail.com>
2024-09-15 17:25:09 -0300

    feat: muda estratégia para aplicar nova configuração do sistema

--- terraform/dev/s3.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "food-cloud-terraform-remote-state"
-    key    = "development/backend.tfstate"
-    # region = var.aws_region
-    # Variables doesn't work here :(
-    region = "us-east-1"
-  }
-}
