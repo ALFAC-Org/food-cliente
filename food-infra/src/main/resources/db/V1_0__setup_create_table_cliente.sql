504a73b35b82dff608ba654adce854436f61b14c
carlohcs <carlohcs@gmail.com>
2024-09-22 00:06:31 -0300

    feat: simplifica migrations

--- food-infra/src/main/resources/db/V1_0__setup_create_table_cliente.sql
+++ food-infra/src/main/resources/db/V1_0__setup_create_table_cliente.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS cliente (
     id int auto_increment  primary key,
     cpf VARCHAR(11) NOT NULL,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    email VARCHAR(100) NOT NULL,
+    uuid BINARY(16) NOT NULL default(UNHEX(REPLACE(UUID(), '-', '')))
 );
 
 create unique index cliente_cpf_uk
@@ -14,7 +15,6 @@ create unique index cliente_cpf_uk
 
 create unique index cliente_email_uk
     on cliente (email);
-
 
 INSERT INTO cliente(cpf, nome, email) VALUES ("11111111111", 'Joaquim Da Silva', 'joaquim@teste.com');
 INSERT INTO cliente(cpf, nome, email) VALUES ("22222222222", 'Maria Joaquim Da Silva', 'maria@teste.com');
